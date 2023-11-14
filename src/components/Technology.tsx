3a1982c43376067633cd5f502f1450455146d17e
PrathamLalwani <prathamlalwani33@gmail.com>
2023-11-14 15:11:05 -0800

    updated project styles

--- src/components/Technology.tsx
+++ src/components/Technology.tsx
@@ -7,7 +7,7 @@ const Technology = (props: Props) => {
   return (
     <div
       title={props.iconName}
-      className="m-2 flex h-max w-max shrink-0 flex-grow-0 basis-0 items-center justify-center rounded-full bg-background-200 bg-opacity-10 p-4 backdrop-blur-xl"
+      className="m-2 flex h-max w-max shrink-0 flex-grow-0 basis-0 items-center justify-center rounded-full bg-background-200 bg-opacity-10 p-4 backdrop-blur-lg"
     >
       <SvgIcon
         iconName={props.iconName}
