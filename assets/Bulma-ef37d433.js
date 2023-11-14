a83b02286631caf0c5c9250cc528038045d539d5
PrathamLalwani <84996242+PrathamLalwani@users.noreply.github.com>
2023-11-14 16:50:27 +0000

    Build: (1314fda) removed redundant imports

--- assets/Bulma-ef37d433.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-584bccf2.js";const r=e=>t.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 128 128",...e},t.createElement("path",{fill:"#00D1B2",d:"M59.2 0l40 40-24 24 32 31.9L59.4 128l-40-39.9 7.7-56z"}));export{r as default};
---
+++ assets/Vercel-d51efa7f.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-593726a0.js";const r=t=>e.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",width:128,height:128,viewBox:"0 0 128 128",...t},e.createElement("path",{style:{fillRule:"nonzero",fill:"#000",fillOpacity:1},d:"M63.984 17.184 127.964 128H0Zm0 0"}));export{r as default};
