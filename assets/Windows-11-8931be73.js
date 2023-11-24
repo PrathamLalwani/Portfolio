26a69b13c72ccef1539cb993a9fe44726d8c749e
PrathamLalwani <84996242+PrathamLalwani@users.noreply.github.com>
2023-11-24 00:15:25 +0000

    Build: (42b7773) icon update

--- assets/Windows-11-8931be73.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-a1dd5ca2.js";const o=e=>t.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 128 128",...e},t.createElement("path",{fill:"#0078d4",d:"M67.328 67.331h60.669V128H67.328zm-67.325 0h60.669V128H.003zM67.328 0h60.669v60.669H67.328zM.003 0h60.669v60.669H.003z"}));export{o as default};
